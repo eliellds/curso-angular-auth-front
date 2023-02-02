1fd31d0baf46e38f60e25320e0f51ac1d5486f9e
Eliel Lopes Dos Santos <ellsantos@rd.com.br>
2023-02-01 23:04:19 -0300

    estudos de autenticacao e protecao de rotas para login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Guard
+import { AuthGuard } from './core/guards/auth.guard';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    loadChildren: () => import('./core/components/auth/auth.module')
+    .then(m => m.AuthModule),
+  },
+  {
+    path: 'admin',
+    canActivateChild: [AuthGuard],
+    loadChildren: () => import('./components/admin/admin.module')
+    .then(m => m.AdminModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, map, Observable, throwError } from 'rxjs';
+
+//Services
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url: string = 'http://localhost:3000';
+
+  constructor(
+    private http: HttpClient, private router: Router
+    ) { }
+
+  public signIn(payLoad: { email: string, password: string }): Observable<any> {
+    return this.http.post<{token: string}>(`${this.url}/sign`, payLoad).pipe(
+      map((res) => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', res.token);
+        return this.router.navigate(['admin']);
+      }),
+      catchError((err) => {
+        if (err.error.message) {
+          return throwError(() => err.error.message);
+        }
+
+        return throwError(() => "No momento, não é possível validar os dados informados. Tente novamente mais tarde.");
+      })
+    );
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
+  }
+
+  public isAuthenticated(): boolean {
+
+    const token = localStorage.getItem('access_token');
+
+    if (!token) {
+      return false;
+    }
+
+    const jwtHelper = new JwtHelperService();
+
+    return !jwtHelper.isTokenExpired(token);
+  }
+
+}
